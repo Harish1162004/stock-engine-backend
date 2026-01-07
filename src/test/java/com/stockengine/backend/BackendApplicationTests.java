6f9544583decb7b35c2e329bfb47c3f8be32a98c
Harish1162004 <harish1162004@gmail.com>
2026-01-07 14:37:37 +0530

    Fix datasource config and tests

--- src/test/java/com/stockengine/backend/BackendApplicationTests.java
+++ src/test/java/com/stockengine/backend/BackendApplicationTests.java
@@ -3,11 +3,16 @@ package com.stockengine.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+		properties = {
+				"spring.datasource.url=jdbc:h2:mem:testdb",
+				"spring.datasource.driver-class-name=org.h2.Driver",
+				"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+		}
+)
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
